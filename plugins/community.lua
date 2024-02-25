1c07f562a9dd413b88246046f0d268e4c97f6e25
shelken <shelken.pxk@gmail.com>
2024-02-26 01:57:25 +0800

    config notify and noice and config luasnip and add some plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -3,12 +3,22 @@ return {
   "AstroNvim/astrocommunity",
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+
+  -- theme
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  -- language
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.go" },
-  { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.yaml" },
   { import = "astrocommunity.pack.toml" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.docker" },
+
+  -- 自动补全命令
+  { import = "astrocommunity.completion.cmp-cmdline"},
+  -- replaces the UI for messages, cmdline and the popupmenu
+  -- https://github.com/folke/noice.nvim
+  { import = "astrocommunity.utility.noice-nvim"},
+  
 }
