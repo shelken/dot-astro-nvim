01fb7621cc458d6f0a872bc2a31aa1b644cfdbaf
shelken <shelken.pxk@gmail.com>
2024-03-18 14:20:48 +0800

    add comment-box

--- mappings.lua
+++ mappings.lua
@@ -33,6 +33,7 @@ return {
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
   },
+  v = {},
   t = {
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
--- plugins/user.lua
+++ plugins/user.lua
@@ -53,4 +53,22 @@ return {
       },
     },
   },
+  -- comment-box
+  {
+    "LudoPinelli/comment-box.nvim",
+    keys = {
+      {
+        mode = "v",
+        "<leader>mm",
+        ":CBccbox",
+        desc = "美化注释",
+      },
+      {
+        mode = "n",
+        "<leader>mm",
+        ":CBccbox",
+        desc = "美化注释",
+      },
+    },
+  },
 }
