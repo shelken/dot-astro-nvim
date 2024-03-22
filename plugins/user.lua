e1f476acd903d756c964acc1cbd34db9609cf0d2
shelken <shelken.pxk@gmail.com>
2024-03-22 17:05:39 +0800

    add autosave plugin

--- plugins/user.lua
+++ plugins/user.lua
@@ -10,6 +10,16 @@ return {
   --   end,
   -- },
 
+  -- auto save
+  {
+    "0x00-ketsu/autosave.nvim",
+    -- lazy-loading on events
+    event = { "InsertLeave", "TextChanged" },
+    opts = function(_, opts)
+      opts.prompt_style = "stdout" -- notify or stdout
+    end,
+  },
+
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
