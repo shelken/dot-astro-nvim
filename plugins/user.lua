2ebea93af48a9181aea9ad2aa634c0c8392331d1
shelken <shelken.pxk@gmail.com>
2024-04-14 21:59:28 +0800

    ✨ add yazi plugin

--- plugins/user.lua
+++ plugins/user.lua
@@ -113,4 +113,15 @@ return {
       }
     end,
   },
+  -- yazi
+  {
+    "DreamMaoMao/yazi.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim",
+    },
+    keys = {
+      { "<leader>py", "<cmd>Yazi<CR>", desc = "Toggle Yazi" },
+    },
+  },
 }
