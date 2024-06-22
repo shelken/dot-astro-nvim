1cd72cbc821fc23b3da359d60a370a6d5f863276
shelken <shelken.pxk@gmail.com>
2024-06-22 14:37:54 +0800

    ✨ 引入pastify插件

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -2,6 +2,9 @@
 return {
   "0x00-ketsu/markdown-preview.nvim",
   ft = { "md", "markdown", "mkd", "mkdn", "mdwn", "mdown", "mdtxt", "mdtext", "rmd", "wiki" },
+  keys = {
+    { noremap = true, mode = "n", "<leader>mp", "<cmd>MPToggle<CR>", desc = "切换md预览" },
+  },
   config = function()
     require("markdown-preview").setup {
       -- your configuration comes here
