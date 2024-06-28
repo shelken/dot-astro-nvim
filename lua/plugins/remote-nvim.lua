f72d099c624d55d70a281b3ee1bca171ae687af0
shelken <shelken.pxk@gmail.com>
2024-06-28 22:08:22 +0800

    ♻️ change remote nvim

--- lua/plugins/remote-nvim.lua
+++ lua/plugins/remote-nvim.lua
@@ -1,11 +1,36 @@
 ---@type LazySpec
 return {
-  "amitds1997/remote-nvim.nvim",
-  version = "v0.3.2", -- Pin to GitHub releases
-  dependencies = {
-    "nvim-lua/plenary.nvim", -- For standard functions
-    "MunifTanjim/nui.nvim", -- To build the plugin UI
-    "nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
+  "nosduco/remote-sshfs.nvim",
+  dependencies = { "nvim-telescope/telescope.nvim" },
+  opts = {
+    -- Refer to the configuration section below
+    -- or leave empty for defaults
   },
-  config = true,
+  -- keys = {
+  --   { noremap = true, mode = "n", "<leader>sc", "<cmd>PastifyAfter<CR>", desc = "粘贴文件引用" },
+  -- },
+  -- config = function()
+  --   local api = require "remote-sshfs.api"
+  --   vim.keymap.set("n", "<Leader>sc", api.connect, { desc = "连接远程文件" })
+  --   vim.keymap.set("n", "<Leader>sd", api.disconnect, { desc = "断开远程文件" })
+  --   vim.keymap.set("n", "<leader>se", api.edit, {})
+  --
+  --   -- (optional) Override telescope find_files and live_grep to make dynamic based on if connected to host
+  --   local builtin = require "telescope.builtin"
+  --   local connections = require "remote-sshfs.connections"
+  --   vim.keymap.set("n", "<Leader>ff", function()
+  --     if connections.is_connected then
+  --       api.find_files()
+  --     else
+  --       builtin.find_files()
+  --     end
+  --   end, { desc = "find files" })
+  --   vim.keymap.set("n", "<Leader>fw", function()
+  --     if connections.is_connected then
+  --       api.live_grep()
+  --     else
+  --       builtin.live_grep()
+  --     end
+  --   end, { desc = "find files content" })
+  -- end,
 }
