9524ed9868fa64428e67ac7f06aa118c5067e09d
shelken <shelken.pxk@gmail.com>
2025-03-06 15:34:37 +0800

    🔧 config git

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -54,14 +54,14 @@ return {
       },
     },
   },
-  {
-    "sindrets/diffview.nvim",
-    cmd = { "DiffviewOpen", "DiffviewFileHistory" },
-    keys = {
-      { "<leader>Do", "<cmd>DiffviewOpen<cr>", desc = "DiffView Open" },
-      { "<leader>Dc", "<cmd>DiffviewClose<cr>", desc = "DiffView Close" },
-      { "<leader>Dh", "<cmd>DiffviewFileHistory %<cr>", desc = "DiffView History" },
-    },
-    config = function() require "custom.config.diffview" end,
-  },
+  -- {
+  --   "sindrets/diffview.nvim",
+  --   cmd = { "DiffviewOpen", "DiffviewFileHistory" },
+  --   keys = {
+  --     { "<leader>Do", "<cmd>DiffviewOpen<cr>", desc = "DiffView Open" },
+  --     { "<leader>Dc", "<cmd>DiffviewClose<cr>", desc = "DiffView Close" },
+  --     { "<leader>Dh", "<cmd>DiffviewFileHistory %<cr>", desc = "DiffView History" },
+  --   },
+  --   config = function() require "custom.config.diffview" end,
+  -- },
 }
