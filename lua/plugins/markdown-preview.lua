992e9a33d8febbb2173070e91eaf845b0f03a97b
shelken <shelken.pxk@gmail.com>
2025-03-12 12:55:57 +0800

    fix markdown show images

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,22 +1,22 @@
 ---@type LazySpec
 return {
   -- better markdown preview
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    keys = {
-      {
-        noremap = true,
-        mode = "n",
-        "<leader>mp",
-        "<cmd>MarkdownPreviewToggle<CR>",
-        desc = "切换md预览(浏览器)",
-      },
-    },
-    ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
-    -- build = "cd app && yarn install",
-  },
+  -- {
+  --   "iamcco/markdown-preview.nvim",
+  --   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  --   keys = {
+  --     {
+  --       noremap = true,
+  --       mode = "n",
+  --       "<leader>mp",
+  --       "<cmd>MarkdownPreviewToggle<CR>",
+  --       desc = "切换md预览(浏览器)",
+  --     },
+  --   },
+  --   ft = { "markdown" },
+  --   build = function() vim.fn["mkdp#util#install"]() end,
+  --   -- build = "cd app && yarn install",
+  -- },
   {
     "MeanderingProgrammer/render-markdown.nvim",
     dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
@@ -25,5 +25,21 @@ return {
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
     opts = {},
+  },
+  {
+    -- show image in markdown
+    "3rd/image.nvim",
+    opts = {
+      backend = "kitty",
+      processor = "magick_rock",
+      markdown = {
+        enabled = true,
+        clear_in_insert_mode = false,
+        download_remote_images = true,
+        only_render_image_at_cursor = true,
+        floating_windows = false, -- if true, images will be rendered in floating markdown windows
+        filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+      },
+    },
   },
 }
