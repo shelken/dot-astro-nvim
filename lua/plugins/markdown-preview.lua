01cc781efded51495130f1ca66aac3219a51dde8
shelken <shelken.pxk@gmail.com>
2025-06-23 13:28:50 +0800

    fix

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -26,20 +26,20 @@ return {
     ---@type render.md.UserConfig
     opts = {},
   },
-  {
-    -- show image in markdown
-    "3rd/image.nvim",
-    opts = {
-      backend = "kitty",
-      processor = "magick_rock",
-      markdown = {
-        enabled = true,
-        clear_in_insert_mode = false,
-        download_remote_images = true,
-        only_render_image_at_cursor = true,
-        floating_windows = false, -- if true, images will be rendered in floating markdown windows
-        filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
-      },
-    },
-  },
+  -- {
+  --   -- show image in markdown
+  --   "3rd/image.nvim",
+  --   opts = {
+  --     backend = "kitty",
+  --     processor = "magick_rock",
+  --     markdown = {
+  --       enabled = true,
+  --       clear_in_insert_mode = false,
+  --       download_remote_images = true,
+  --       only_render_image_at_cursor = true,
+  --       floating_windows = false, -- if true, images will be rendered in floating markdown windows
+  --       filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+  --     },
+  --   },
+  -- },
 }
